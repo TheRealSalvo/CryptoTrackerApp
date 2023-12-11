f024cafce0017ddf930c47dc823c17b6d9af6826
p-yates <148437369+p-yates@users.noreply.github.com>
2023-12-11 12:07:23 +0100

    add my code

--- CryptoTracker/ContentView.swift
+++ CryptoTracker/ContentView.swift
@@ -31,19 +31,80 @@ struct Sparkline: Shape{
 }
 
 struct ContentView: View {
+    
+    @ObservedObject var viewModel: MarketOverviewVewModel
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "bitcoinsign.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                VStack{
+                    Text("Crypto Tracker: NC2")
+                    Text("Powered by CoinGecko").font(.footnote)
+                }
+            }
+            
+            .padding()
+            Sparkline(points: prices).path(in: CGRect(x: 0, y: 100, width: 250.0, height: 100.0))
+                .stroke(.black, lineWidth: 10.0)
+            
+            List(viewModel.coins) { coin in
+                NavigationLink {
+                    //TO DO insert a link to the coin detail view
+                } label: {
+                    HStack{
+                        Text(coin.name)
+                        Text(String(coin.currentPrice))
+                        //Text(String((coin.sparkline?.price[0])!)) //jsut to prove we were accessing the data
+                    }
+                }
+            }
         }
-        .padding()
-        Sparkline(points: prices).path(in: CGRect(x: 0, y: 100, width: 250.0, height: 100.0))
-            .stroke(.black, lineWidth: 10.0)
+        .onAppear {
+            viewModel.updateCoins()
+        }
     }
 }
 
-#Preview {
-    ContentView()
-}
+//struct MarketOverviewView_Previews: PreviewProvider {
+//    static var previews: some View {
+//
+//        let viewModel = MarketOverviewVewModel()
+//
+//        viewModel.coins = [
+//            MarketData(
+//                id: "bitcoin",
+//                symbol: "btc",
+//                name: "Bitcoin",
+//                image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+//                currentPrice: 39887.0,
+//                marketCap: 782690443079.0,
+//                marketCapRank: 1,
+//                totalVolume: 25358705984.0,
+//                high24h: 40953.0,
+//                low24h: 39832.0,
+//                priceChange24h: -1058.582579497248,
+//                priceChangePercentage24h: -2.58537,
+//                marketCapChange24h: -15875580666.482422,
+//                marketCapChangePercentage24h: -1.98801,
+//                circulatingSupply: 19563537.0,
+//                totalSupply: 21000000.0,
+//                maxSupply: 21000000.0,
+//                ath: 59717.0,
+//                athChangePercentage: -33.00595,
+//                athDate: "2021-11-10T14:24:11.849Z",
+//                atl: 51.3,
+//                atlChangePercentage: 77888.23046,
+//                atlDate: "2013-07-05T00:00:00.000Z",
+//                lastUpdated: "2023-12-07T19:47:58.627Z"
+//            )
+//        ]
+//
+//        return Group {
+//            MarketOverviewView(viewModel: viewModel)
+//        }
+//    }
+
+
--- CryptoTracker/Models/MarketOverviewViewModel.swift
+++ CryptoTracker/Models/MarketOverviewViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class MarketOverviewVewModel: ObservableObject {
+    
+    @Published var coins = [MarketData]()
+    var currency: Currency = .dollars
+
+    let decoder = JSONDecoder()
+
+    func getMarketData() async throws -> [MarketData] {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?order=market_cap_desc&per_page=100&page=1&vs_currency=usd&sparkline=true") else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let marketData = try JSONDecoder().decode([MarketData].self, from: data)
+        return marketData
+    }
+    
+    init() {
+        updateCoins()
+    }
+
+    func updateCoins() {
+        Task { @MainActor in
+            do {
+                coins = try await getMarketData()
+                print(coins[0])
+            } catch let error {
+                print("Error: \((error))")
+            }
+        }
+    }
+}
