9a1fb087f3f32f90125aa69454bf4163597d6cb0
TheRealSalvo <salva0729@gmail.com>
2023-12-14 09:58:32 +0100

    added initializer CardViewModel from [MarketData]

--- CryptoTracker/ViewModels/CardViewModel.swift
+++ CryptoTracker/ViewModels/CardViewModel.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 class CardViewModel {
-    var cards : [Card] = [
-        Card(name: "BTC", value: 23.45, imageSymbol: Image("bitcoin"), marketCap: "MarketCap 43.75$B", volume: "Volume 24h 31,60$B", priceChangePercentage24h: "0,56"),
-        Card(name: "SOL", value: 23.45, imageSymbol:  Image("solana"), marketCap: "MarketCap 43.75$B", volume: "Volume 24h 31,60$B", priceChangePercentage24h: "0,56"),
-        Card(name: "ETH", value: 23.45, imageSymbol: Image("ethereum"), marketCap: "MarketCap 43.75$B", volume: "Volume 24h 31,60$B", priceChangePercentage24h: "+0,56")
-    ]
+    var cards : [Card] = []
+    
+    init(coins: [MarketData]){
+        for coin in coins {
+            cards.append(Card(coin: coin))
+        }
+    }
 }
 
 
