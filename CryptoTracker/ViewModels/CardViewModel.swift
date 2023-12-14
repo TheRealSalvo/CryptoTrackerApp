a7cff1c756e35db18d17fd7968565b888cef225a
TheRealSalvo <salva0729@gmail.com>
2023-12-14 09:24:27 +0100

    refactored CardViewModel

--- CryptoTracker/ViewModels/CardViewModel.swift
+++ CryptoTracker/ViewModels/CardViewModel.swift
@@ -1,11 +1,10 @@
 import SwiftUI
-class CardViewModal {
-
-    var Cards = [
-        CardModel(Name: "BTC", Value: 23.45, ImageSymbol: Image("bitcoin"), MarketCap: "MarketCap 43.75$B", Volume: "Volume 24h 31,60$B", priceChangePercentage24h: "0,56"),
-        CardModel(Name: "SOL", Value: 23.45, ImageSymbol:  Image("solana"), MarketCap: "MarketCap 43.75$B", Volume: "Volume 24h 31,60$B", priceChangePercentage24h: "0,56"),
-        CardModel(Name: "ETH", Value: 23.45, ImageSymbol: Image("ethereum"), MarketCap: "MarketCap 43.75$B", Volume: "Volume 24h 31,60$B", priceChangePercentage24h: "+0,56")
-]
+class CardViewModel {
+    var cards : [Card] = [
+        Card(name: "BTC", value: 23.45, imageSymbol: Image("bitcoin"), marketCap: "MarketCap 43.75$B", volume: "Volume 24h 31,60$B", priceChangePercentage24h: "0,56"),
+        Card(name: "SOL", value: 23.45, imageSymbol:  Image("solana"), marketCap: "MarketCap 43.75$B", volume: "Volume 24h 31,60$B", priceChangePercentage24h: "0,56"),
+        Card(name: "ETH", value: 23.45, imageSymbol: Image("ethereum"), marketCap: "MarketCap 43.75$B", volume: "Volume 24h 31,60$B", priceChangePercentage24h: "+0,56")
+    ]
 }
 
 
