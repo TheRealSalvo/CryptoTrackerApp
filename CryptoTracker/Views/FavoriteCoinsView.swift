262aa8081b6915864d24dac2a19487c48ea9950e
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2023-12-15 16:48:38 +0100

    added the func of search bar

--- CryptoTracker/Views/FavoriteCoinsView.swift
+++ CryptoTracker/Views/FavoriteCoinsView.swift
@@ -17,42 +17,49 @@ struct FavoriteCoinsView: View {
     @State private var showSheet = false
     @State private var searchText = ""
     
-    //CHECK IF I USEED / NEEDED THIS
     let alertTitle: String = "Api error"
     
+    // Computed property to filter favorite coins based on search text
+    private var filteredCoins: [FavoriteCoin] {
+        if searchText.isEmpty {
+            return favouriteCoins
+        } else {
+            return favouriteCoins.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+
     var body: some View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
-                
                 LazyVGrid(columns: [GridItem(.fixed(360))], spacing: 7) {
-                    ForEach(favouriteCoins) { coin in
+                    // Use the filtered list here
+                    ForEach(filteredCoins) { coin in
                         let data = viewModel.coins.first { viewModelData in
                             viewModelData.name == coin.name
                         }
                         
-                        CardView(card: Card(coin: data!))
-                            .contextMenu(menuItems: {
-                                Button(action: {
-                                    removeFromFavourite(coin: coin)
-                                }, label: {
-                                    Text("Remove from Favourites")
+                        if let data = data {
+                            CardView(card: Card(coin: data))
+                                .contextMenu(menuItems: {
+                                    Button(action: {
+                                        removeFromFavourite(coin: coin)
+                                    }, label: {
+                                        Text("Remove from Favourites")
+                                    })
                                 })
-                                
-                            })
+                        }
                     }
                 }
             }
             .scrollClipDisabled()
             .padding()
             .navigationTitle("Favourite Coins")
-           
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showSheet.toggle()
                     } label: {
                         Image(systemName:"plus.circle")
-                           
                     }
                     .sheet(isPresented: $showSheet) {
                         AllCoinsListView(viewModel: self.viewModel, function: addToFavorites)
@@ -62,31 +69,30 @@ struct FavoriteCoinsView: View {
         }
         .searchable(text: $searchText)
         .refreshable {
-                viewModel.updateCoins()
-            }
+            viewModel.updateCoins()
+        }
         .alert(
             "Error",
             isPresented: $viewModel.showAPIAlert) {
-                    Button("OK", role: .cancel) { }
-                    } message: {
-                        Text("Error \(String(describing: viewModel.alertContentString))")
-                        }
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text("Error \(String(describing: viewModel.alertContentString))")
+            }
     }
     
-    func addToFavorites (coin: String){
+    func addToFavorites(coin: String) {
         let newFavoriteCoin = FavoriteCoin(name: coin)
         modelContext.insert(newFavoriteCoin)
     }
     
-    func removeFromFavourite(coin: FavoriteCoin){
+    func removeFromFavourite(coin: FavoriteCoin) {
         modelContext.delete(coin)
     }
 }
 
-
-#Preview {
-    FavoriteCoinsView(
-        viewModel: MarketOverviewViewModel()
-    )
+// Preview
+struct FavoriteCoinsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteCoinsView(viewModel: MarketOverviewViewModel())
+    }
 }
-
